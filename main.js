680c1b44d10121d6afee9b9bbabad98244a34b70
nijjarjatt <nijjar.login@gmail.com>
2015-10-21 16:36:56 -0400

    added intial framework setup

---
+++ main.js
@@ -0,0 +1,15 @@
+(function (){
+    this.TaleroidPopUp = function(){
+        //Global plugin data
+
+        //Private methods
+
+    }
+    
+    //public methods
+    TaleroidPopUp.prototype.method1 = function(){
+       //Method 1 Logic goe shere
+    }
+}());
+
+var popup = new TaleroidPopUp();
